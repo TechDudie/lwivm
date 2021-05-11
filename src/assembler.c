56340c2e52cf3684ba5bb85eedec8760fd594d0d
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-11 11:58:43 -0500

    Update assembler.c

--- src/assembler.c
+++ src/assembler.c
@@ -5,12 +5,12 @@ int main(int argc, char *argv[]) {
     ptr = fopen(argv[1],"r");
     fread(program,sizeof(program), 1, ptr);
     char line[16][65536];
-    char *ptr;
+    char *str;
     int counter;
-    ptr = strtok(program,"\n");
-    while (ptr != NULL) {
-        snprintf(line[counter], 16, "%s", ptr);
-        ptr = strtok(NULL, "\n");
+    str = strtok(program,"\n");
+    while (str != NULL) {
+        snprintf(line[counter], 16, "%s", str);
+        str = strtok(NULL, "\n");
         counter++;
     }
     return 0;
